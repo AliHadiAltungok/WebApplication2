494f70dbd4d6b99033dddf36335910433187f436
Ali <altungokalihadi@gmail.com>
2022-06-12 22:22:04 +0300

    Add project files.

---
+++ WebApplication2/Controllers/StudentController.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        public JsonResult Get()
+        {
+
+            string query = @"select ID,Name,Surname,Department from dbo.Student";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+        [HttpPost]
+        public JsonResult Post(Student student)
+        {
+            string query = @"
+                    insert into dbo.Student (Name,Surname,Department) values 
+                    ('" + student.Name + @"','" + student.Surname + @"','" + student.Department + @"')
+                    ";
+
+
+            DataTable dt = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dt.Load(sqlDataReader); ;
+
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+
+
+
+        [HttpPut]
+        public JsonResult Put(Student student)
+        {
+            string query = @"
+                    update dbo.Student set 
+                    Name = '" + student.Name + @"',Surname = '" + student.Surname + @"',Department = '" + student.Department + @"'
+                    where ID = " + student.ID + @" 
+                    ";
+
+
+            DataTable dt = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dt.Load(sqlDataReader); ;
+
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from dbo.Student
+                    where ID = " + id + @" 
+                    ";
+
+
+            DataTable dt = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("StudentAppCon");
+            SqlDataReader sqlDataReader;
+            using (SqlConnection sqlConnection = new SqlConnection(sqlDataSource))
+            {
+                sqlConnection.Open();
+                using (SqlCommand sqlCommand = new SqlCommand(query, sqlConnection))
+                {
+                    sqlDataReader = sqlCommand.ExecuteReader();
+                    dt.Load(sqlDataReader); ;
+
+                    sqlDataReader.Close();
+                    sqlConnection.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+
+    }
+}
---
+++ WebApplication2/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Department { get; set; }
+    }
+}
